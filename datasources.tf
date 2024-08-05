cedc51c1b4be8ca70fde09d5808590b1ddf57534
Phil Cuff <phil.cuff@aderant.com>
2024-08-05 19:35:01 +1200

    Switch to using tfe_outputs

--- datasources.tf
+++ datasources.tf
@@ -7,6 +7,6 @@ data "aws_ssm_parameter" "amzn2_linux" {
 }
 
 data "tfe_outputs" "networking" {
-  organization = var.tfe.organization
+  organization = var.tfe_organization
   workspace    = var.tfe_workspace_name
 }
